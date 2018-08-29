4d76b8c691a550663c57644f3f5b8a13973f48bd
walter.gomero <walter.gomero@hp.com>
2018-08-29 17:53:24 -0500

    Added Gallery Folder

---
+++ eGallery.Web.Razor/Pages/app/Gallery/Upload.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eGallery.Web.Razor.Pages.app.Gallery
+{
+    public class UploadModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
